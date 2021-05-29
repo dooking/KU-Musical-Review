804f8c65a688002031c417f9824fcdb7271556d0
scw3812 <dlffpraos@naver.com>
2021-05-29 20:40:01 +0900

    first commit

--- client/src/components/musical/index.jsx
+++ client/src/components/musical/index.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/media-has-caption */
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+import * as S from './style';
 
 function Musical() {
-    return <div>Musical</div>;
+    const [MusicalData, setMusicalData] = useState(null);
+
+    useEffect(() => {
+        Axios.get(`/musical/1`)
+            .then((response) => {
+                console.log(response.data);
+                setMusicalData(response.data);
+            });
+    }, []);
+
+    return (
+        <S.MainContainer>
+            {MusicalData ? <div>sdfs</div> : <div>zzz</div>}
+        </S.MainContainer>
+    );
 }
 
 export default Musical;
